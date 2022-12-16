dc9e28a402b8c0d4418ef9302e8db901609c4db6
charconstpointer <attwistingnether@gmail.com>
2022-12-16 14:43:02 +0100

    chore: gen

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"context"
+
+	apiv1 "github.com/charconstpointer/ihateannotations/proto/gen/go/api/v1"
+)
+
 func main() {
+	c := apiv1.NewApiServiceClient(nil)
+	res, err := c.Foo(context.TODO(), nil, nil)
+	if
 }
