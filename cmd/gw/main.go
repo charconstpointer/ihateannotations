0bb9f0f6da990b0c6bb05c6d5b378bb944f2ca84
charconstpointer <attwistingnether@gmail.com>
2022-12-17 15:23:49 +0100

    chore: handle gw

---
+++ cmd/gw/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"flag"
+	apiv1 "github.com/charconstpointer/ihateannotations/proto/gen/go/api/v1"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var (
+	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:8080", "gRPC server endpoint")
+)
+
+// can handle both grpc and rest on the same port
+func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// This is a partial recreation of gRPC's internal checks https://github.com/grpc/grpc-go/pull/514/files#diff-95e9a25b738459a2d3030e1e6fa2a718R61
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			otherHandler.ServeHTTP(w, r)
+		}
+	})
+}
+
+func main() {
+	flag.Parse()
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	gmux := runtime.NewServeMux()
+	mux := http.NewServeMux()
+	mux.Handle("/", gmux)
+
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := apiv1.RegisterApiServiceHandlerFromEndpoint(ctx, gmux, *grpcServerEndpoint, opts)
+	if err != nil {
+		log.Fatalf("Failed to register gateway: %v", err)
+	}
+	http.ListenAndServe(":8081", mux)
+}
